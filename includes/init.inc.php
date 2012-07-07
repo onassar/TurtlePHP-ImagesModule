84f44b8f032b798e2c586a14dfcf80524ae5ecd8
Oliver Nassar <onassar@gmail.com>
2012-07-07 13:18:14 -0400

    Ups

--- includes/init.inc.php
+++ includes/init.inc.php
@@ -7,17 +7,13 @@
         $info = pathinfo(__DIR__);
         $parent = $info['dirname'];
 
-        // perform requirement checks
-        require_once 'requirements.inc.php';
-
         // include class, controller
         require_once ($parent) . '/Images.class.php';
         require_once ($parent) . '/controllers/Images.class.php';
 
-        // config
+        // flow includes
         require_once 'config.inc.php';
-
-        // custom module routing
+        require_once 'requirements.inc.php';
         require_once 'routes.inc.php';
     };
 
